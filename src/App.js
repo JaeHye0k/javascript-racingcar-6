3ee31f1c46d106446d925a68f6ae6e1ca72822ae
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-07-26 08:58:56 +0900

    feat(App,Car): 입력 기능 추가
    사용자로부터 자동차 이름과 시도할 횟수 입력 받기
    - N개의 자동차 이름 입력(이름은 쉼표 기준으로 구분)
    - 각각의 자동차 이름을 갖는 객체 생성
    - 시도할 횟수 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import Car from "./Car.js";
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+	constructor() {
+		this.cars = [];
+		this.count = 0;
+	}
+	async play() {
+		this.input();
+	}
+
+	async input() {
+		let carNames = await Console.readLineAsync(
+			"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+		);
+		carNames = carNames.split(",");
+		for (const name of carNames) {
+			this.cars.push(new Car(name));
+		}
+		let tryCount = +(await Console.readLineAsync(
+			"시도할 횟수는 몇 회인가요?\n"
+		));
+	}
 }
 
 export default App;
---
+++ src/Car.js
@@ -0,0 +1,12 @@
+/**
+ * @property {String} name 자동차 이름
+ * @property {Number} moveCount 전진 횟수
+ */
+class Car {
+	constructor(name) {
+		this.name = name;
+		this.moveCount = 0;
+	}
+}
+
+export default Car;
