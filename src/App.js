7a2aed4bc4aab2eebec66e5c5e5af915ef84c97c
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-07-30 18:45:02 +0900

    feat(App): 메인 로직 기능 추가
    - race : 하나의 자동차를 기준으로 한 회차에 수행할 기능(랜덤 숫자 추출, 앞으로 이동) 수행
    - getRandomNumber : 랜덤 숫자 추출
    - isMoveFoward : 앞으로 가야하는지 판단
    - getWinner : 최종 우승자 추출

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ import { isValidCarName, isValidTryCount } from "./validators.js";
 class App {
 	constructor() {
 		this.cars = [];
-		this.count = 0;
+		this.tryCount = 0;
 	}
 	async play() {
 		await this.input();
+		Console.print("\n실행 결과");
+		for (let i = 0; i < this.tryCount; i++) {
+			this.cars.forEach((car) => {
+				this.race(car);
+				Console.print(car.output); // 각 차의 실행 결과 출력
+			});
+			Console.print(""); // 한 줄 띄우기
+		}
+		const winners = this.getWinner(this.cars);
+		Console.print(`최종 우승자 : ${winners.join(", ")}`);
 	}
 
 	async input() {
@@ -19,7 +29,36 @@ class App {
 
 		let tryCount = await Console.readLineAsync(MESSAGES.INPUT.TRY_COUNT);
 		isValidTryCount(tryCount); // 시도할 횟수 유효성 검사
-		this.count = +tryCount;
+		this.tryCount = +tryCount;
+	}
+
+	race(car) {
+		const num = this.getRandomNumber();
+		const moveFoward = this.isMoveFoward(num);
+		if (moveFoward) car.moveFoward();
+	}
+
+	getRandomNumber() {
+		return Random.pickNumberInRange(0, 9);
+	}
+
+	isMoveFoward(num) {
+		return num >= 4;
+	}
+
+	getWinner(cars) {
+		const winners = [];
+		let max = 0;
+		cars.forEach(({ moveCount, name }) => {
+			if (moveCount > max) {
+				winners.length = 0;
+				winners.push(name);
+				max = moveCount;
+			} else if (moveCount === max) {
+				winners.push(name);
+			}
+		});
+		return winners;
 	}
 }
 
